c21c68755dc9c537baa37217404043867491c0f0
pramodrmurthy <39050809+pramodrmurthy@users.noreply.github.com>
2025-08-31 09:33:20 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   //base: mode === "production" ? "wanderweave-content" : "/",
-  base: "./",
+  base: "mantramiles",
   server: {
     host: "::",
     port: 8080,
